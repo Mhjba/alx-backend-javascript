bf053991ef431e1319c6c70836e4c283addbc859
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-19 13:42:49 +0200

    node js basic

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('./3-read_file_async');
+
+const app = express();
+
+const PORT = 1245;
+const DB_PATH = process.argv.length > 2 ? process.argv[2] : '';
+
+/**
+ * Counts the students in a CSV data file.
+ * @param {String} dataPath The path to the CSV data file.
+ */
+
+app.get('/', (_, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (_, res) => {
+  const responseParts = ['This is the list of our students'];
+
+  countStudents(DB_PATH)
+    .then((report) => {
+      responseParts.push(report);
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    })
+    .catch((err) => {
+      responseParts.push(err instanceof Error ? err.message : err.toString());
+      const responseText = responseParts.join('\n');
+      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
+      res.statusCode = 200;
+      res.write(Buffer.from(responseText));
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
