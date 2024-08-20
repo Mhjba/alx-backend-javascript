09935e09b96b8e9a93ce8b9c2523579be3fc17e8
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-20 11:11:08 +0200

    node js basic

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -2,22 +2,15 @@ const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const PORT = 1245;
-const HOST = 'localhost';
-const app = http.createServer();
 const DB_PATH = process.argv.length > 2 ? process.argv[2] : '';
-
-/**
- * Counts the students in a CSV data file.
- * @param {String} dataPath The path to the CSV data file.
- */
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
     const responseText = 'Hello Holberton School!';
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.setHeader(200, { 'Content-Type': 'text/plain' });
     res.end(responseText);
   } else if (req.url === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.setHeader(200, { 'Content-Type': 'text/plain' });
     res.write('This is the list of our students\n');
 
     countStudents(DB_PATH)
@@ -28,7 +21,7 @@ const app = http.createServer((req, res) => {
         res.end(err.message);
       });
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.setHeader(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
