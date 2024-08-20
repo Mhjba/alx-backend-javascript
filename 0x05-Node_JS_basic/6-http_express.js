f21a5a8e645a84a80f3d72c0509a48c0341d3ace
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-20 10:49:26 +0200

    node js basic

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 
-app.get('/', (request, response) => {
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = app;
