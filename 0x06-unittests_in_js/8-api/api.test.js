31f3c7cec3434edda0458f0d9ec9e23fdfccb23c
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-22 20:06:59 +0200

    unittests in js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,30 +1,13 @@
 const request = require('request');
 const { expect } = require('chai');
-const server = require('./api'); // Import the server
 
-describe('Index page', () => {
-  let serverInstance;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  before((done) => {
-    serverInstance = server.listen(7865, done); // Start server before running tests
-  });
-
-  after((done) => {
-    serverInstance.close(done); // Stop server after tests are done
-  });
-
-  it('should return a 200 status code', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      if (err) return done(err);
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-
-  it('should return the correct response body', (done) => {
-    request.get('http://localhost:7865', (err, res, body) => {
-      if (err) return done(err);
-      expect(body).to.equal('Welcome to the payment system');
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
