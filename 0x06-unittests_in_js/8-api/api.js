57cbad15b8341c91d9a51fd443f37aebc579fc01
Mhjba <mahjoubaaitalla15@gmail.com>
2024-08-22 18:33:53 +0200

    unittests in js

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
 const app = express();
-const PORT = 7865;
 
-app.get('/', (request, resp) => {
-  resp.send('Welcome to the payment system');
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-    console.log("API available on localhost port 7865");
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
 });
 
 module.exports = app;
